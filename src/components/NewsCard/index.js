a46e3b0df0711b9c1e2b77feffbd68e10ef64bc6
MineManiac <matheusbarrosr@gmail.com>
2022-05-31 18:36:08 -0300

    fixed news key issue

--- src/components/NewsCard/index.js
+++ src/components/NewsCard/index.js
@@ -5,12 +5,10 @@ export default function NewsCard(props) {
     const title = props.title
     const url = props.url
     const source = props.source
-    const key = props.key
   return (
           <div className="news-card" >
             <a className = "news-title a" href={url} target="_blank" rel="noreferrer noopener">{title}</a>
             <p className="news-source">Source: {source}</p>
-            <p>{key}</p>
           </div>
   );
 }
