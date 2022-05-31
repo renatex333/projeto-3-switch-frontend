5de8fc7518fbcf81cb2897aecde9306c9d2b2d61
MineManiac <matheusbarrosr@gmail.com>
2022-05-31 17:49:57 -0300

    changes that were made previously

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -2,59 +2,87 @@ import React from "react";
 import FriendsList from "../FriendsList";
 import News from "../News";
 import "./index.css";
-
+import {useState, useEffect} from "react";
+import axios from "axios";
 export default function Homepage(props) {
 
   const props_exemplo = {"data": ["Diogo", "Jorás", "Matheus", "Renatex", "Eduardo", "Ricardo", "Paulo", "Pedro", "Mauro", "Lorena", "Ana", "Joaquina"]};
-  const news_exemplo = [{
-    "title":"Xbox is working on a small device for playing games over the internet",
-    "url":"https://www.independent.co.uk/tech/xbox-microsoft-keystone-streaming-cloud-gaming-console-b2089088.html",
-    "source":"independentUk"
-    },
-    {
-      "title":"Xbox Game Pass’s May 2022 list includes Skate and Sniper Elite 5",
-      "url":"https://www.independent.co.uk/games/xbox-game-pass-games-may-2022-b2080986.html",
-      "source":"independentUk"
-      },
-      {
-        "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
-        "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
-        "source":"independentUk"
-      },
-      {
-        "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
-        "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
-        "source":"independentUk"
-      },
-      {
-        "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
-        "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
-        "source":"independentUk"
-      },
-      {
-        "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
-        "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
-        "source":"independentUk"
-      },
-      {
-        "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
-        "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
-        "source":"independentUk"
-      },
-      {
-        "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
-        "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
-        "source":"independentUk"
-      },
-      {
-        "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
-        "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
-        "source":"independentUk"
-      },
-  ]
+  // const news_exemplo = [{
+  //   "title":"Xbox is working on a small device for playing games over the internet",
+  //   "url":"https://www.independent.co.uk/tech/xbox-microsoft-keystone-streaming-cloud-gaming-console-b2089088.html",
+  //   "source":"independentUk"
+  //   },
+  //   {
+  //     "title":"Xbox Game Pass’s May 2022 list includes Skate and Sniper Elite 5",
+  //     "url":"https://www.independent.co.uk/games/xbox-game-pass-games-may-2022-b2080986.html",
+  //     "source":"independentUk"
+  //     },
+  //     {
+  //       "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
+  //       "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
+  //       "source":"independentUk"
+  //     },
+  //     {
+  //       "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
+  //       "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
+  //       "source":"independentUk"
+  //     },
+  //     {
+  //       "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
+  //       "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
+  //       "source":"independentUk"
+  //     },
+  //     {
+  //       "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
+  //       "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
+  //       "source":"independentUk"
+  //     },
+  //     {
+  //       "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
+  //       "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
+  //       "source":"independentUk"
+  //     },
+  //     {
+  //       "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
+  //       "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
+  //       "source":"independentUk"
+  //     },
+  //     {
+  //       "title":"Xbox Game Pass confirms the seven games leaving in May 2022",
+  //       "url":"https://www.independent.co.uk/games/xbox-game-pass-may-2022-b2081065.html",
+  //       "source":"independentUk"
+  //     },
+  // ]
+
+  const [news, setNews] = useState([])
+
+  const loadNews = (event) => {
+    // event.preventDefault()
+        axios
+          .get(
+              "http://localhost:8000/api/news",
+
+              axios.defaults.xsrfCookieName = 'csrftoken',
+              axios.defaults.xsrfHeaderName = 'X-CSRFToken',
+              {header : {
+                "Origin" : "http://localhost:3000",
+                "Access-Control-Allow-Origin" : "*"
+              }
+            }
+              )
+          .then((res)=>{ 
+            setNews(res.data)
+            console.log(res.data)
+  }
+  )}
+
+  useEffect(() => {
+    loadNews();
+  },[]);
+
   return (
     <div className="homepage">
-      <News dados={news_exemplo}></News>
+      <News key={0} dados={news}></News>
       <FriendsList dados={props_exemplo}></FriendsList>      
     </div>
   );
---
+++ src/components/NewsCard/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+export default function NewsCard(props) {
+    const title = props.title
+    const url = props.url
+    const source = props.source
+    const key = props.key
+  return (
+          <div className="news-card" >
+            <a className = "news-title a" href={url} target="_blank" rel="noreferrer noopener">{title}</a>
+            <p className="news-source">Source: {source}</p>
+            <p>{key}</p>
+          </div>
+  );
+}
